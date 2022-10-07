909f7501be4963664dc03f0faf4f3f2360840a57
minkisim <minkisim@naver.com>
2022-10-07 16:08:29 +0900

    1007 admin code practice

---
+++ src/main/java/com/shop/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,17 @@
+package com.shop.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 관리자 전용 페이지 관리용 Config
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Unauthorized");
+    }
+}
